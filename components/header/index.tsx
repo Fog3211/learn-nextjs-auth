423c51e5ba5c8996cbeb7cb11cae88ea611b93cd
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-09-03 22:43:49 +0800

    feat: add login page

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -28,8 +28,7 @@ export const Header = () => {
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Learn Next Auth</span>
           </a>
           <div className="flex items-center lg:order-2">
-            <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
-            <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
+            <Link href="/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</Link>
           </div>
           <div className="hidden flex-1 pl-8 justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">

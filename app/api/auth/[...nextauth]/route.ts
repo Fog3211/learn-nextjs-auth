3706706df87a14b924b3b36f65c422da97149373
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-09-03 23:14:14 +0800

    feat: add profile page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,41 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GitlibProvider from "next-auth/providers/gitlab"
-import GoogleProvider from "next-auth/providers/google"
-
-const ProviderList = [
-  {
-    clientId: process.env.GITHUB_ID,
-    clientSecret: process.env.GITHUB_SECRET,
-    provider: GithubProvider
-  },
-  {
-    clientId: process.env.GITLAB_ID,
-    clientSecret: process.env.GITLAB_SECRET,
-    provider: GitlibProvider
-  },
-  {
-    clientId: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    provider: GoogleProvider
-  },
-]
-
-const authOptions: NextAuthOptions = {
-  providers: ProviderList
-    .filter(u => (u.clientId && u.clientSecret))
-    // @ts-ignore
-    .map(item => item.provider({
-      clientId: item.clientId,
-      clientSecret: item.clientSecret,
-    })),
-
-  pages: {
-    signIn: 'login',
-    // signOut: 'logout',
-    // newUser: 'register'
-  }
-}
+import NextAuth from "next-auth"
+import { authOptions } from '@/config/auth'
 
 const handler = NextAuth(authOptions)
 
